cd216867811154736d96ef27b2ed1f5c196b1f42
Anton Sergeev <antonsergeevphd@gmail.com>
2022-05-23 23:02:06 +0300

    feat: solve subtask 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirname = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dirname, (error, files) => {
+  if (error) return console.error(error.message);
+  for (const file of files) {
+    fs.stat(path.join(dirname, file), (error, stats) => {
+      if (error) return console.error(error.message);
+      if (stats.isFile()) {
+        console.log(`${path.parse(file).name} - ${path.extname(file)} - ${stats.size}b`);
+      }
+    });
+  }
+});
