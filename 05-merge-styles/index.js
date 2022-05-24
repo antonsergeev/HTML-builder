e760f63a6e2c2b03371900056a44476f75837364
Anton Sergeev <antonsergeevphd@gmail.com>
2022-05-24 03:15:47 +0300

    feat: solve subtask 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+// const sourceDirname = path.join(__dirname, 'styles');
+// const targetFilename = path.join(__dirname, 'project-dist', 'bundle.css');
+
+const sourceDirname = path.join(__dirname, 'test-files', 'styles');
+const targetFilename = path.join(__dirname, 'test-files', 'bundle.css');
+
+fs.readdir(sourceDirname, {withFileTypes: true}, (error, dirents) => {
+  if (error) return console.error(error.message);
+  const filenames = [];
+  for (const dirent of dirents) {
+    if (dirent.isFile() && path.extname(dirent.name) === '.css') {
+      filenames.push(path.join(sourceDirname, dirent.name));
+    }
+  }
+  filenames.forEach((filename) => {
+    fs.readFile(filename, 'utf-8', (error, data) => {
+      if (error) return console.error(error.message);
+      fs.appendFile(targetFilename, data, (error) => {
+        if (error) return console.error(error.message);
+      });
+    });
+  });
+});
